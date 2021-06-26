69ef586e904d827c35653ac0e836326faed39596
Kallardian <paulobertanha03@gmail.com>
2021-06-26 11:51:42 -0300

    Mais items para lista

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,8 +4,11 @@ import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
 
 import { Profile } from "../../components/Profile";
+import { ListDivider } from "../../components/ListDivider";
 import { ListHeader } from "../../components/ListHeader";
 import { styles } from "./styles"
+import { Appointment } from "../../components/Appointment";
+
 
 export function Home() {
     const [category, setCategory] = useState('')
@@ -13,6 +16,18 @@ export function Home() {
     const appointments = [
         {
             id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challanger sem perder uma partida da md10'
+        },
+        {
+            id: '2',
             guild: {
                 id: '1',
                 name: 'Lendários',
@@ -35,28 +50,26 @@ export function Home() {
                 <Profile />
                 <ButtonAdd />
             </View>
-
-            <View>
-                <CategorySelect
-                    categorySelected={category}
-                    setCategory={handleCategorySelect}
+            <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
+            <View style={styles.content}>
+                <ListHeader
+                    title='Partidas Agendadas'
+                    subtitle="Total 6"
                 />
-                <View style={styles.content}>
-                    <ListHeader
-                        title='Partidas Agendadas'
-                        subtitle="Total 6"
-                    />
 
-                    <FlatList
-                        data={appointments}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) => (
-                            <Text>
-                                {item.guild.name}
-                            </Text>
-                        )}
-                    />
-                </View>
+                <FlatList
+                    data={appointments}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <Appointment data={item} />
+                    )}
+                    ItemSeparatorComponent={() => <ListDivider />}
+                    style={styles.matches}
+                    showsVerticalScrollIndicator={false}
+                />
             </View>
         </View>
     );
